f271d26f6ce08dbbeb9296bc9626998e0055f82a
y11katsuyama <yuuki_katsuyama@mri.co.jp>
2022-05-19 12:58:56 +0900

    fix components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { 
+  NavBar,ActionCardCollection,MarketingFooter
+} from './ui-components';
 
 function App() {
   return (
     <div className="App">
+        <NavBar width={"100vw"}/>
+        <ActionCardCollection />
+        <MarketingFooter width={"100vw"}/>
     </div>
   );
 }
